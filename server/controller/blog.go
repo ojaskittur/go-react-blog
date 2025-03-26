b6bde69984d59287740cd79ffa2e8cda94713f45
ojaskittur <ojaskittur@gmail.com>
2025-03-26 21:51:08 +0530

    implemented BlogList endpoint

--- server/controller/blog.go
+++ server/controller/blog.go
@@ -1,11 +1,19 @@
 package controller
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"GO-REACT-BLOG/server/database"
+	"GO-REACT-BLOG/server/model"
+)
 
 func BlogList(c *fiber.Ctx) error{
 	context :=fiber.Map{
 		"statusText": "OK",
 		"msg": "Blog List",
 	}
+	db :=database.DBConn
+	var records []model.Blog
+	db.Find(&records)
+	context["blog_records"]= records
 	c.Status(200)
 	return c.JSON(context)
 }
--- server/model/blog.go
+++ server/model/blog.go
@@ -2,8 +2,9 @@ package model
 
 // import "gorm.io/gorm"
 
-type Blog struct{
-	ID uint `json:"title" gorm:"primaryKey" null`
-	Title string `json:"title" gorm: "not null;column:title;size:255"`
-	Post string `json:"post" gorm:"not null;column:post;size:255"`
+
+type Blog struct {
+	ID    uint   `json:"id" gorm:"primaryKey;autoIncrement"`
+	Title string `json:"title" gorm:"not null;column:title;size:255"`
+	Post  string `json:"post" gorm:"not null;column:post;size:255"`
 }
