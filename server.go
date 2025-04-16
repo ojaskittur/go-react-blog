99554fd953bdf01a349a8e77dbc821dfd29ed23e
ojaskittur <ojaskittur@gmail.com>
2025-04-17 02:46:49 +0530

    Initialized react app for frontend development

--- server.go
+++ server.go
@@ -5,6 +5,7 @@ import(
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"GO-REACT-BLOG/server/database"
 	"GO-REACT-BLOG/server/router"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 
@@ -19,6 +20,12 @@ func main(){
 	}
 	defer sqlDb.Close()
 	app :=fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "Origin, Content-Type,Accept",
+	}))
+	
 	app.Use(logger.New())
 	router.SetUpRoutes(app)
 	app.Listen(":8000")
